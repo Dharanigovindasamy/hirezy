4df15545404fbe8dee516de08d1912f361656b5e
Dharani Govindhasamy <dharanigkd02@gmail.com>
2024-08-21 16:31:32 +0530

    inital commit

---
+++ src/main/java/com/ideas2it/hirezy/HirezyApplication.java
@@ -0,0 +1,13 @@
+package com.ideas2it.hirezy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HirezyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HirezyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ideas2it/hirezy/HirezyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ideas2it.hirezy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HirezyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
