30cab1ec07a2306e00a4ba30a7b32052596dde5c
Audhayy <audhithiyah2002@gmail.com>
2024-08-22 11:12:08 +0530

    added company and jobPost

---
+++ src/main/java/com/ideas2it/hirezy/model/Company.java
@@ -0,0 +1,33 @@
+package com.ideas2it.hirezy.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "companies")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Company {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(length = 500)
+    private String description;
+
+    @Column(nullable = false)
+    private String location;
+
+    @Column(unique = true)
+    private String website;
+
+//    @OneToMany(mappedBy = "company", cascade = CascadeType.ALL, orphanRemoval = true)
+//    private Set<JobPosting> jobPostings = new HashSet<>();
+}
---
+++ src/main/java/com/ideas2it/hirezy/model/JobPost.java
@@ -0,0 +1,35 @@
+package com.ideas2it.hirezy.model;
+
+
+import java.time.LocalDate;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "job_postings")
+public class JobPost {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(length = 1000)
+    private String description;
+
+    @Column(nullable = false)
+    private LocalDate postedDate;
+
+//    @ManyToOne
+//    @JoinColumn(name = "company_id", nullable = false)
+//    private Company company;
+
+}
