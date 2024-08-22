cad29d1f01a13c314e473d73e4f169b32b4e7c65
kishorekumar .u.n <kk90054@gmail.com>
2024-08-22 11:07:16 +0530

    model added for jobcategory

---
+++ src/main/java/com/ideas2it/hirezy/model/JobCategory.java
@@ -0,0 +1,31 @@
+package com.ideas2it.hirezy.model;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "JobCategory")
+public class JobCategory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name", nullable = false, length = 100)
+    private String name;
+
+    @Column(name = "description", length = 200)
+    private String description;
+
+    @OneToMany(mappedBy = "jobCategory", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Set<JobSubcategory> subcategories;
+
+}
---
+++ src/main/java/com/ideas2it/hirezy/model/JobSubcategory.java
@@ -0,0 +1,30 @@
+package com.ideas2it.hirezy.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "JobSubcategory")
+public class JobSubcategory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name", nullable = false, length = 100)
+    private String name;
+
+    @Column(name = "description", length = 200)
+    private String description;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "job_category_id", nullable = false)
+    private JobCategory jobCategory;
+
+}
