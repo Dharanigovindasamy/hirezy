ea92cec1133e5086e21778b98c418cc1c30b600a
dharani.govindhasamy <dharani.govindhasamy@ideas2it.com>
2024-08-23 16:05:40 +0530

    Fix userconttroller

--- src/main/java/com/ideas2it/hirezy/controller/UserController.java
+++ src/main/java/com/ideas2it/hirezy/controller/UserController.java
@@ -11,27 +11,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api/v1/users")
 public class UserController {
-    @Autowired
-    private LoginService loginService;
-
-    @PostMapping("/signup")
-    public ResponseEntity<APIResponse> signUp(@RequestBody UserDto userDto){
-
-        APIResponse apiResponse = loginService.signUp(signUpRequestDTO);
-
-        return ResponseEntity
-                .status(apiResponse.getStatus())
-                .body(apiResponse);
-    }
-
-    @PostMapping("/login")
-    public ResponseEntity<APIResponse> login(@RequestBody LoginRequestDTO loginRequestDTO ){
-
-        APIResponse apiResponse = loginService.login(loginRequestDTO);
-
-        return ResponseEntity
-                .status(apiResponse.getStatus())
-                .body(apiResponse);
-    }
 
 }
